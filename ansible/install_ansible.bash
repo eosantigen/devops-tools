7854a5bfca0f59a4be29714d266f20743a3efb95
Eos Antigen <eos.antigen@gmail.com>
2022-04-23 15:43:36 +0300

    organized Ansible.

--- ansible/install_ansible.bash
+++ ansible/install_ansible.bash
@@ -8,13 +8,8 @@ if [ -x /sbin/apk ]; then
     apk update && apk add py3-pynacl musl-dev gcc gcc-dev libffi-dev py3-cffi openssl-dev python3
 
 elif [ -x /usr/bin/apt ]; then
-    apt install -y python3 python3-pip openssl-dev python3-cffi libffi-dev #.......TODO : Check packages.
-
-elif [ -x /usr/bin/dnf || /usr/bin/yum ]; then
-    # check updated commanline for this distro.
-    echo "NEEDS FIXING";
+    apt install -y python3-pip libssl-dev python3-cffi libffi-dev
+else
+    echo "NO PACKAGE MANAGER FOUND.\n(required for installing Ansible dependencies.)" && exit 1;
 fi
-
-echo "NO PACKAGE MANAGER FOUND.\n(required for installing Ansible dependencies.)" && exit 1;
-
-#python3 -m pip install ansible=${ANSIBLE_VERSION}
+python3 -m pip install ansible==${ANSIBLE_VERSION}
