4d18d5bb7c039b16320a8d2461a5f44de08a6ad7
Eos Antigen <eos.antigen@gmail.com>
2023-01-15 12:23:40 +0200

    adding tag values in Tag model.
    via Migration.

--- apps/python/django/devops/logbook/models.py
+++ apps/python/django/devops/logbook/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+
 class Task(models.Model):
 
-    user = models.CharField(max_length=33)
-    time = models.CharField(max_length=33)
+    user = models.CharField(max_length=30)
+    time = models.CharField(max_length=30)
     task = models.CharField(max_length=500)
     tags = models.CharField(max_length=100, default="-", null=True)
+
+    class Meta:
+        ordering = ('-time',)
+
+class TagManager(models.Manager):
+    def populate_tags(self, values):
+        for i in values:
+            self.create(tag=i)
+        # do something with the book
+        # return i
 
 class Tag(models.Model):
 
@@ -14,6 +25,24 @@ class Tag(models.Model):
 
     tag = models.CharField(max_length=20, default="-", primary_key=True)
 
-    # class Meta:
-    #     verbose_name = "tag"
-    #     verbose_name_plural = "tags"
+    objects = TagManager()
+
+    class Meta:
+        verbose_name = "tag"
+        verbose_name_plural = "tags"
+
+
+# book = Book.objects.create_book("Pride and Prejudice")    
+
+TAG_VALUES = {
+'aks',
+'prod',
+'dev',
+'cicd',
+'hardware',
+}
+
+Tag.objects.populate_tags(TAG_VALUES)
+
+# for tag in TAG_VALUES:
+#     t = Tag.objects.populate_tags(tag)
