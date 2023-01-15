e471c1e81e50aae8ec2c9c843894f0082e2495c7
Eos Antigen <eos.antigen@gmail.com>
2023-01-15 10:22:00 +0200

    apps/logbook initial setup success.
    adding values for tags via migration.

--- apps/python/django/devops/logbook/models.py
+++ apps/python/django/devops/logbook/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+
+    user = models.CharField(max_length=33)
+    time = models.CharField(max_length=33)
+    task = models.CharField(max_length=500)
+    tags = models.CharField(max_length=100, default="-", null=True)
+
+class Tag(models.Model):
+
+    # def __str__(self):
+    #     return self.tag
+
+    tag = models.CharField(max_length=20, default="-", primary_key=True)
+
+    # class Meta:
+    #     verbose_name = "tag"
+    #     verbose_name_plural = "tags"
